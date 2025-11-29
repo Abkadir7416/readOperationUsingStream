bd1ca6610ea94c33aea8bc6640485b87cd25d9db
Abdul Kadir <abdulk0749@gmail.com>
2025-11-29 12:34:08 +0530

    write txt file from scratch on server starts

--- readFileSlowly.js
+++ readFileSlowly.js
@@ -4,15 +4,19 @@ const fs = require('fs');
 
 const readStream = fs.createReadStream('text.txt',{highWaterMark:4})
 
+let count=0;
 readStream.on('data', (chunck)=>{
   // console.log('chunk;; ', chunck);
-  console.log('length:;; ', chunck.length)
-  console.log('End of Loop::');
-  fs.appendFileSync('new-text.txt', chunck);
+  if(count==0){
+    fs.writeFileSync('new-text.txt', chunck);
+  }
+  else{
+    fs.appendFileSync('new-text.txt', chunck);
+  }
   readStream.pause();
   setTimeout(() => {
     readStream.resume();
   }, 1000);
-
+  count++;
 })
 console.log('End of Execution::');
